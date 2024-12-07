6f79b6cf345052b0df78fc606b358fe62d0039eb
Starinin Andrey <starinin-andrey@ya.ru>
2024-12-07 14:20:03 +0300

    Update

--- Calculator/MainWindow.axaml.cs
+++ Calculator/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using MathEvaluation.Extensions;
 
 namespace Calculator;
 
@@ -7,5 +9,33 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void ButtonDigit_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Input.Text += (sender as Button)?.Content;
+    }
+
+    private void ButtonArithmetic_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Input.Text += $" {(sender as Button)?.Content} ";
+    }
+
+    private void ButtonEqual_OnClick(object? sender, RoutedEventArgs e)
+    {
+        var result = Input.Text!.Evaluate();
+        Output.Text += $"{Input.Text} = {result}\n";
+
+        Input.Clear();
+    }
+
+    private void ButtonAllClear_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Input.Clear();
+        Output.Text = string.Empty;
+    }
+    private void ButtonClear_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Input.Clear();
     }
 }
